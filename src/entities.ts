3392f0a307c2eb0bc5869ed3bcef16181e82ad5e
COOL <team@cool-js.com>
2025-01-15 20:55:44 +0800

    兼容二进制打包

--- src/entities.ts
+++ src/entities.ts
@@ -1,25 +1,2 @@
 // 自动生成的文件，请勿手动修改
-import * as entity0 from './modules/plugin/entity/info';
-import * as entity1 from './modules/base/entity/sys/user_role';
-import * as entity2 from './modules/base/entity/sys/user';
-import * as entity3 from './modules/base/entity/sys/role_menu';
-import * as entity4 from './modules/base/entity/sys/role_department';
-import * as entity5 from './modules/base/entity/sys/role';
-import * as entity6 from './modules/base/entity/sys/param';
-import * as entity7 from './modules/base/entity/sys/menu';
-import * as entity8 from './modules/base/entity/sys/log';
-import * as entity9 from './modules/base/entity/sys/department';
-import * as entity10 from './modules/base/entity/sys/conf';
-export const entities = [
-  ...Object.values(entity0),
-  ...Object.values(entity1),
-  ...Object.values(entity2),
-  ...Object.values(entity3),
-  ...Object.values(entity4),
-  ...Object.values(entity5),
-  ...Object.values(entity6),
-  ...Object.values(entity7),
-  ...Object.values(entity8),
-  ...Object.values(entity9),
-  ...Object.values(entity10),
-];
+export const entities = [];
