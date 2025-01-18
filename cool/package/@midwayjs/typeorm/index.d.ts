95f7218d9e629ab59eb61dd6717ab1421c9f695a
COOL <team@cool-js.com>
2025-01-18 15:16:05 +0800

    清理

--- cool/package/@midwayjs/typeorm/index.d.ts
+++
@@ -1,9 +0,0 @@
-import { typeormConfigOptions } from './dist/index';
-
-export * from './dist/index';
-
-declare module '@midwayjs/core/dist/interface' {
-  interface MidwayConfig {
-    typeorm?: PowerPartial<typeormConfigOptions>;
-  }
-}
--- src/modules/base/db/tenant.ts
+++ src/modules/base/db/tenant.ts
@@ -2,9 +2,13 @@ import { EventSubscriberModel } from '@midwayjs/typeorm';
 import { EntitySubscriberInterface, TransactionCommitEvent } from 'typeorm';
 import { BeforeQueryEvent } from 'typeorm/subscriber/event/QueryEvent';
 import * as _ from 'lodash';
+import { App, IMidwayApplication } from '@midwayjs/core';
 
 @EventSubscriberModel()
 export class TenantSubscriber implements EntitySubscriberInterface<any> {
+  @App()
+  app: IMidwayApplication;
+
   /**
    * 查询前
    * @param event
@@ -12,6 +16,7 @@ export class TenantSubscriber implements EntitySubscriberInterface<any> {
   // @ts-ignore
   beforeQuery(event: BeforeQueryEvent<any>, query: string, parameters: any[]) {
     if (_.startsWith(event.query, 'SELECT')) {
+      console.log('event.query', this.app);
       query = 'SELECT * FROM "tenant11"';
       //   // 参数
       //   const parameters = event.parameters;
